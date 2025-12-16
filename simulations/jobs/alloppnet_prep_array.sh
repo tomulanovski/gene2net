d1d4802f9862d30be6b126b91691fd4e97034df7
tomulanovski <tom.ulanovski@gmail.com>
2025-12-16 23:02:56 +0800

    fixing

--- simulations/jobs/alloppnet_prep_array.sh
+++ simulations/jobs/alloppnet_prep_array.sh
@@ -223,12 +223,8 @@ conda activate alloppnet || {
     exit 1
 }
 
-# Add trailing slash to INPUT_DIR for AlloppDT path construction
-# AlloppDT concatenates paths without separator, so we need the trailing /
-INPUT_DIR_WITH_SLASH="${INPUT_DIR}/"
-
 Rscript "${SCRIPTS_DIR}/generate_beast_xml.r" \
-    "${INPUT_DIR_WITH_SLASH}" \
+    "${INPUT_DIR}" \
     "${OUTPUT_DIR}"
 
 if [ $? -ne 0 ]; then
